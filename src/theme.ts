129bc9df17f687cb803702532ffa205fad24c785
sli9 <sil9gosy@mail.ru>
2024-05-08 23:55:34 +0300

    added colors in theme object

--- src/theme.ts
+++ src/theme.ts
@@ -10,5 +10,10 @@ export const theme = createTheme({
             h3: {fontSize: '20px', fontWeight: '600'} // heading for title of movie
         },
     },
+    colors: {
+        gray: ['#FFFFFF', '#F5F5F6', '#EAEBED', '#D5D6DC', '#ACADB9', '#7B7C88', '#232134', '', '', ''],
+        purple: ['#F2ECFA', '#E5D5FA', '#D1B4F8', '#BD93F7', '#9854F6', '#541F9D', '', '', '', ''],
+        yellow: ['#FAB005', '', '', '', '', '', '', '', '', ''],
+    },
     defaultRadius: '8px',
 });
